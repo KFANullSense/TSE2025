c2e6f739551150c8cb0c136e2e358f543c1c483a
KFANullSense <79997026+KFANullSense@users.noreply.github.com>
2025-05-06 13:37:43 +0100

    added star values based on score

--- Scripts/gameEndScene.gd
+++ Scripts/gameEndScene.gd
@@ -2,6 +2,25 @@ extends Node2D
 
 func _ready() -> void:
 	get_node("CanvasLayer/Score").text = str(GameManager.currentScore)
+	updateStarSprite()
 
 func mainMenu():
 	GameManager.ReturnToMainMenu()
+
+func updateStarSprite():
+	var currentScore: int = GameManager.currentScore
+	var scoreThresholds: Array[int] = GameManager.loadedLevelValues.LEVEL_VALUES[GameManager.currentLevelIndex].starThresholds
+	
+	var starTexture: Texture2D
+	
+	if (currentScore >= scoreThresholds[2]):
+		starTexture = SpriteLoader.threeStarSprite
+	elif (currentScore >= scoreThresholds[1]):
+		starTexture = SpriteLoader.twoStarSprite
+	elif (currentScore >= scoreThresholds[0]):
+		starTexture = SpriteLoader.oneStarSprite
+	else:
+		starTexture = SpriteLoader.zeroStarSprite
+	
+	get_node("CanvasLayer/StarSprites").texture = starTexture
+	
--- Scripts/spriteLoader.gd
+++ Scripts/spriteLoader.gd
@@ -4,3 +4,8 @@ var tomatoSprite: Texture2D = preload("res://UI Images/Food Items/tomato.png")
 var onionSprite: Texture2D = preload("res://UI Images/Food Items/Onion.png")
 var mushroomSprite: Texture2D = preload("res://UI Images/Food Items/Mushroom.png")
 var soupSprite: Texture2D = preload("res://UI Images/Food Items/SoupT.png")
+
+var zeroStarSprite: Texture2D = preload("res://UI Images/Stars/0 star.png")
+var oneStarSprite: Texture2D = preload("res://UI Images/Stars/1 star.png")
+var twoStarSprite: Texture2D = preload("res://UI Images/Stars/2 star.png")
+var threeStarSprite: Texture2D = preload("res://UI Images/Stars/3 star.png")
